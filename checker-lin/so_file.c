bac7a8f362a31552189012f862e9203122ecb62b
Andrei250 <dumi.andrei0808@gmail.com>
2022-03-26 13:35:56 +0200

    Implemented fseek and fflush

--- checker-lin/so_file.c
+++ checker-lin/so_file.c
@@ -5,23 +5,43 @@
 
 #define BUFFSIZ 4096 // as specified in the problem statement
 
+#define NO_OPERATION 0
+#define WRITE_OPERATION 1
+#define READ_OPERATION 2
+
 typedef struct _so_file {
     int fp;
     int flags;
+    int error;
+    int eof;
+    int index;
+    int sz;
 
     char buffer[BUFFSIZ];
-
+    char op;
 } SO_FILE;
 
 
 SO_FILE *structInit() {
     SO_FILE *str = malloc(sizeof(struct _so_file));
+    
+    str->eof = 0;
+    str->error = 0;
+    str->op = NO_OPERATION;
+    str->sz = BUFFSIZ;
+    str->index = 0;
 
     return str;
 }
 
 void freeStructure(SO_FILE *structure) {
     free(structure);
+}
+
+void resetBuffer(SO_FILE *structure) {
+    memset(structure->buffer, 0, BUFFSIZ);
+    structure->index = 0;
+    structure->sz = BUFFSIZ;
 }
 
 FUNC_DECL_PREFIX SO_FILE *so_fopen(const char *pathname, const char *mode) {
@@ -82,4 +102,87 @@ FUNC_DECL_PREFIX HANDLE so_fileno(SO_FILE *stream) {
 }
 #endif
 
+FUNC_DECL_PREFIX int so_fflush(SO_FILE *stream) {
+    if (stream->op != WRITE_OPERATION) {
+        return SO_EOF;
+    }
+
+    int written = 0, totalWritten = 0;
+
+    while (totalWritten < stream->index) {
+        written = write (stream->fp,
+            stream->buffer + totalWritten,
+            stream->index - totalWritten);
+
+        if (written < 0) {
+            stream->error = SO_EOF;
+            return SO_EOF;
+        }
+
+        totalWritten += written;
+    }
+
+    resetBuffer(stream);
+
+    return 0;
+}
+
+FUNC_DECL_PREFIX int so_fseek(SO_FILE *stream, long offset, int whence) {
+    if (stream->op == READ_OPERATION) {
+        resetBuffer(stream);
+    } else if (stream->op == WRITE_OPERATION) {
+        so_fflush(stream);
+    }
+
+    int rc = lseek(stream->fp, offset, whence);
+
+    if (rc < 0) {
+        return SO_EOF;
+    }
+
+    return 0;
+}
+
+FUNC_DECL_PREFIX long so_ftell(SO_FILE *stream) {
+    return 0;
+}
+
+FUNC_DECL_PREFIX int so_feof(SO_FILE *stream) {
+    return stream->eof;
+}
+
+FUNC_DECL_PREFIX int so_ferror(SO_FILE *stream) {
+    return stream->error;
+}
+
+FUNC_DECL_PREFIX int so_fgetc(SO_FILE *stream) {
+    stream->op = READ_OPERATION;
+
+    if (stream->index > 0 &&
+        stream->index < stream->sz) { // if we still have chars in buffer
+        return (int) ((unsigned char) stream->buffer[stream->index++]);
+    }
+
+    stream->sz = read(stream->fp, stream->buffer, BUFFSIZ);
+
+    if (stream->sz <= 0) {
+        stream->eof = 1;
+
+        if (stream->sz == 0) {
+            stream->error = SO_EOF;
+        }
+
+        return SO_EOF;
+    }
+
+    stream->index = 0;
+
+    return (int) ((unsigned char) stream->buffer[stream->index++]);
+}
+
+FUNC_DECL_PREFIX int so_fputc(int c, SO_FILE *stream) {
+    stream->op = WRITE_OPERATION;
+
+    return SO_EOF;
+}
 
